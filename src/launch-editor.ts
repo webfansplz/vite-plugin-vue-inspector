4c4212dc644f5ac5c3ec8d4e16398ab239cfe0fb
webfansplz <308241863@qq.com>
2022-05-20 14:36:34 +0800

    refactor(launch-editor): use `powershell` instead of `wmic`

--- src/launch-editor.ts
+++ src/launch-editor.ts
@@ -211,20 +211,22 @@ function guessEditor() {
       }
     }
     else if (process.platform === "win32") {
-      // Some processes need elevated rights to get its executable path.
-      // Just filter them out upfront. This also saves 10-20ms on the command.
       const output = child_process
-        .execSync(
-          "wmic process where \"executablepath is not null\" get executablepath",
-          { stdio: ["pipe", "pipe", "ignore"] },
-        )
+        .execSync("powershell -Command \"Get-Process | Select-Object Path\"", {
+          stdio: ["pipe", "pipe", "ignore"],
+        })
         .toString()
       const runningProcesses = output.split("\r\n")
       for (let i = 0; i < runningProcesses.length; i++) {
-        const processPath = runningProcesses[i].trim()
-        const processName = path.basename(processPath)
-        if (COMMON_EDITORS_WIN.indexOf(processName) !== -1)
-          return [processPath]
+      // `Get-Process` sometimes returns empty lines
+        if (!runningProcesses[i])
+          continue
+
+        const fullProcessPath = runningProcesses[i].trim()
+        const shortProcessName = path.basename(fullProcessPath)
+
+        if (COMMON_EDITORS_WIN.indexOf(shortProcessName) !== -1)
+          return [fullProcessPath]
       }
     }
     else if (process.platform === "linux") {
