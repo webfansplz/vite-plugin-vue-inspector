c2c0af0bd6b171bf346582ffc01c370ebb97c487
webfansplz <308241863@qq.com>
2023-09-09 12:33:10 +0800

    feat: support `decorators` and `import-attributes` syntax

---
+++ packages/playground/vue2/src/Count.vue
@@ -0,0 +1,23 @@
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class MyComponent extends Vue {
+  @Prop() readonly message!: string
+  @Prop({ default: 0 }) count!: number
+
+  increment() {
+    this.count++
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>Message: {{ message }}</p>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">
+      Increment
+    </button>
+  </div>
+</template>
