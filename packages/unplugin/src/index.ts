062d4d9cb6bae151f9cf7612f6581c220cd79f44
Anthony Fu <anthonyfu117@hotmail.com>
2023-10-02 14:15:46 +0800

    fix: unplugin

--- packages/unplugin/src/index.ts
+++ packages/unplugin/src/index.ts
@@ -2,7 +2,16 @@ import { createUnplugin } from 'unplugin'
 import VitePluginInspector from 'vite-plugin-vue-inspector'
 import type { Options } from './types'
 
-export default createUnplugin<Options>(options => ({
-  name: 'unplugin-vue-inspector',
-  vite: VitePluginInspector(options) as any,
-}))
+export default createUnplugin<Options, true>((options) => {
+  const plugins = VitePluginInspector(options) as any
+  return [
+    {
+      name: 'unplugin-vue-inspector',
+      vite: plugins[0],
+    },
+    {
+      name: 'unplugin-vue-inspector:post',
+      vite: plugins[1],
+    },
+  ]
+})
