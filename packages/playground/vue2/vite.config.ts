aec0fa7a965c4979de6238d5471b567b36774595
zhangqihui <zhangqihui@gigacloudtech.com>
2024-12-06 13:36:42 +0800

    fix(playground): vue2 template modify

--- packages/playground/vue2/vite.config.ts
+++ packages/playground/vue2/vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
+import vue from '@vitejs/plugin-vue2'
+import vueJsx from '@vitejs/plugin-vue2-jsx'
 import Inspector from 'unplugin-vue-inspector/vite'
 
 export default defineConfig({
   plugins: [
-    createVuePlugin({
-      jsx: true,
-      jsxOptions: {
-        compositionAPI: true,
-      },
+    vueJsx({
+      babelPlugins: [['@babel/plugin-proposal-decorators', { legacy: true }], ['@babel/plugin-proposal-class-properties', { loose: true }]],
     }),
+    vue(),
     Inspector({
       vue: 2,
       toggleButtonVisibility: 'always',
